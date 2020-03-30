fb9275a97e9b392e499e5b0fa5aef4f0ff7d5c94
Kalliopi Mavridou <mavridaki52@gmail.com>
2020-03-31 00:21:58 +0300

    Some changes in database and mongoTesting added.

--- mongo/exampleMongoConnection.py
+++ mongo/exampleMongoConnection.py
@@ -5,100 +5,15 @@ from BasicClasses.Agenda import *
 import mongo.user
 
 database = "lcThessaloniki"
-
 url = """mongodb://{}:{}@116.203.85.249/{}""".format(mongo.user.username, mongo.user.password, database)
 
-### Egw toy eipa na ta svisoume ta data!!!
-data = {
-    "date": date.today().strftime("%d/%m/%Y"),
-    "lc": "thessaloniki",
-    "sections": [
-        {
-            "section_name": "gmElections",
-            "topics": [
-                {
-                "topic_name": "openGmstaffEl",
-                "votable": "True",
-                "yes_no_vote": "True",
-                "open_ballot": "False"
-                },
-                {
-                "topic_name": "MinutesElection",
-                "votable": "True",
-                "yes_no_vote": "False",
-                "possible_answers": ["Marios", "Tasos", "urMOM"
-                    ],
-                "open_ballot": "True"
-                }
-            ]
-        },
-        {
-            "section_name": "WorkShop",
-            "topics": [
-                {
-                "topic_name": "openVote",
-                "votable": "True",
-                "yes_no_vote": "True",
-                "open_ballot": "False"
-                },
-                {
-                "topic_name": "Future of Workshop",
-                "votable": "True",
-                "yes_no_vote": "False",
-                "possible_answers": ["Cancelation", "Postpone", "procced"],
-                "open_ballot": "True"
-                }
-            ]
-        },
-        {
-            "section_name": "Krasia",
-            "topics": [
-                {
-                    "topic_name": "openVote",
-                    "votable": "True",
-                    "yes_no_vote": "True",
-                    "open_ballot": "False"
-                },
-                {
-                    "topic_name": "Wanna go ?",
-                    "votable": "True",
-                    "yes_no_vote": "True",
-                    "open_ballot": "True"
-                },
-                {
-                    "topic_name": "Where",
-                    "votable": "True",
-                    "yes_no_vote": "False",
-                    "possible_answers": ["Omprella, Podhlato, SomeWeirdAssPlace"
-                    ],
-                    "open_ballot": "True"
-                },
-                {
-                    "topic_name": "Lets go",
-                    "votable": "False"
-                }
-            ]
-        }
-    ]
-}
-data2 = {
-    'id': '2',
-    'lc': 'thessaloniki',
-    'date': date.today().strftime("%d/%m/%Y"),
-    'sections': [  # this will be a list of objects  but for now lets assume that there are title, subtitle
-        {
-            'title': 'this is the first topic',
-            'subtitle': 'this is its subtitle1'
-        },
-        {
-            'title': 'this is the second topic',
-            'subtitle': 'this is its subtitle2'
-        }
-    ]
-}
-
 def agendaJsonToAgendaObject(agenda_json, agenda_id):
-    """"""
+    """
+    Converts Agenda Json to an Object
+    :param agenda_json:
+    :param agenda_id:
+    :return: Agenda Object
+    """
     sections = []
     jsonSection = list(agenda_json.get("sections"))
     for sec in jsonSection:
@@ -113,31 +28,40 @@ def agendaJsonToAgendaObject(agenda_json, agenda_id):
     object = Agenda(agenda_json.get("date"), agenda_json.get("lc"), agenda_id, sections)
     return object
 
+def print_agenda(agenda):
+    """
+    Prints requested agenda
+    :param agenda:
+    :return: Agenda
+    """
+    print(agenda.date, agenda.id, agenda.lc, agenda.sections)
 
 class connectMongo:
 
     def __init__(self):
         """
-        connect to database with credentials and get an object of mongoclient
+        Connection to database with credentials and get an object of mongoclient
         """
         self.client = MongoClient(url, authSource="admin")['lcThessaloniki']
         self.db = self.client.lcThessaloniki
 
     def getAllDatabasesFromLC(self):
         """
-        :return: all the databases from this lc
+        Returns all databases from this LC
+        :return: database
         """
         return self.client.list_collection_names()
 
     def getAllAgendas(self):
         """
+        Returns all agendas from this database
         :return: a cursor to all agendas
         """
         return self.db.agendas.find()
 
     def createNewAgenda(self, json_agenda):
         """
-        adds a new agenda to database
+        Adds a new agenda to database
         :param json_agenda
         :return: added agenda object
         """
@@ -170,7 +94,7 @@ class connectMongo:
         Replaces an agenda with agenda_id, with new_agenda
         :param agenda_id:
         :param new_agenda:
-        :return:
+        :return: agenda object
         """
         return self.db.agendas.update_one({'_id': ObjectId(agenda_id)}, {'$set': new_agenda})
 
@@ -205,7 +129,7 @@ class connectMongo:
 
     def createNewTopic(self, agenda_id, section_position, topic_position, topic_json):
         """
-        Adds new topic to existing agenda in requested position
+        Adds a new topic to existing agenda in requested position
         :param agenda_id:
         :param section_position:
         :param topic_position:
@@ -222,9 +146,9 @@ class connectMongo:
 
     def deleteAgenda(self, agenda_id):
         """
-        Deletes an agenda with agenda_id
+        Deletes an agenda with agenda_id from the database
         :param agenda_id:
-        :return:
+        :return: agenda object
         """
         return self.db.agendas.delete_many({'id': agenda_id})
 
@@ -257,52 +181,11 @@ class connectMongo:
         self.db.agendas.update_one({'_id': ObjectId(agenda_id)}, {'$set': object.makeJson()})
         return object
 
-
     def deleteAll(self):
         """
-        Deletes all agendas from database
-        :return:
+        Deletes all agendas from the database
+        :return: empty list
         """
         return self.db.agendas.drop()
 
-
-def print_agenda(agenda):
-    print(agenda.date, agenda.id, agenda.lc, agenda.sections)
-
-"""
 mongo = connectMongo()
-
-a = mongo.createNewAgenda(data)
-print_agenda(a)
-
-
-mongo.updateAgenda(a.id, data)
-b = mongo.getAgendaById(a.id)
-print_agenda(b)
-
-
-mongo.createNewSectionInPosition(a.id, 'Krasiaaaaaa', 0)
-b = mongo.getAgendaById(a.id)
-print_agenda(b)
-
-mongo.createNewTopic(b.id,0,0,{'topic_name': 'openGmstaffEl', 'votable': 'True', 'yes_no_vote': 'True', 'open_ballot': 'False'})
-c = mongo.getAgendaById(b.id)
-print_agenda(c)
-
-mongo.deleteSection(a.id,1)
-d=mongo.getAgendaById(a.id)
-print_agenda(d)
-
-mongo.deleteSection(a.id,0)
-d = mongo.getAgendaObjectById(a.id)
-print_agenda(d)
-
-mongo.deleteTopic(a.id,0,0)
-e = mongo.getAgendaObjectById(a.id)
-print_agenda(e)
-
-mongo.deleteAll()
-
-s = mongo.getAllAgendas()
-print(list(s))
-"""
