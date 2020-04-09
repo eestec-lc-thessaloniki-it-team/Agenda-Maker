e8ae00c39d26dd3dc34c5271d55cb5347e03d3a0
Katerina <yeorgiadou.k@gmail.com>
2020-04-09 13:59:13 +0300

    Improvement in flaskTesting

--- Tests/flaskTesting.py
+++ Tests/flaskTesting.py
@@ -5,13 +5,28 @@ from BasicClasses.Agenda import *
 
 class FlaskTesting(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.basic_url = "http://127.0.0.1:5000/"
+        params = {"date": "27-06-2020", "lc": "Thessaloniki"}
+        response = requests.post(self.basic_url + "create-agenda", json=params)
+        self.id = response.json().get("agenda").get("id")
+
     def test_createAgenda(self):
         self.basic_url = "http://127.0.0.1:5000/"
         params = {"date": "27-06-2020", "lc": "Thessaloniki"}
         response = requests.post(self.basic_url + "create-agenda", json=params)
-        data = response.json()
-        print(data)
+        self.assertEqual(response.json().get("response"), 200)
 
+        params = {"date": "27-06-2020"}
+        response = requests.post(self.basic_url + "create-agenda", json=params)
+        self.assertEqual(response.json().get("response"), 400)
+
+        params = {"lc": "Thessaloniki"}
+        response = requests.post(self.basic_url + "create-agenda", json=params)
+        self.assertEqual(response.json().get("response"), 400)
+
+    def tearDown(self) -> None:
+      pass
 
 if __name__ == '__main__':
     unittest.main()
